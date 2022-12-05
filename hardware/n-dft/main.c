6111812c118c8808b54491c8dc3c0e3a4836b542
e-lab innovations <mohammedashad23@gmail.com>
2022-12-05 15:50:33 +0530

    N-DFT code fixed

--- hardware/n-dft/main.c
+++ hardware/n-dft/main.c
@@ -7,7 +7,7 @@
 #include<math.h>
 int main(void) {
     float *Xn, *YnReal, *YnImg;
-    int *Xlen ,*N;
+    int *Xlen, *N;
     Xn=(float *)0x80010000;       //input x(n)
     YnReal=(float *)0x80011000;       //real part of output
     YnImg=(float *)0x80012000;       //img part of output
@@ -16,22 +16,23 @@ int main(void) {
     int i,k,n,len;
     n=*N;
     len=*Xlen;
-    for(k=0;k<n;k++)
-    {
+    for(k=0;k<n;k++) {
         YnReal[k]=0;
         YnImg[k]=0;
 
     }
-    while(len>n); //Stop program
 
-    for(k=len;k<n-len;k++) {
+//    while(len>n); //Stop program
+
+    for(k=len;k<n;k++) {
       Xn[k]=0;
     }
     for(k=0;k<n;k++) {
         for(i=0;i<n;i++) {
-            YnReal[k]=YnReal[k]+Xn[k]*cos(2*3.14*k*i/n);
-             YnImg[k]=YnImg[k]+Xn[k]*sin(2*3.14*k*i/n);
+           YnReal[k]=YnReal[k]+(Xn[i]*cos(2*3.14*k*i/n));
+           YnImg[k]=YnImg[k]-(Xn[i]*sin(2*3.14*k*i/n));
         }
     }
-	return 0;
+
 }
+
